47389b47d9dfd0411ae6ff8585d3cb077db59589
Ian Rogers <therazzberry25@gmail.com>
2025-09-26 20:55:17 -0400

    Update invincible_effect.gd

--- scenes/dev/ian_r/invincible_effect.gd
+++ scenes/dev/ian_r/invincible_effect.gd
@@ -7,6 +7,7 @@ func _init(_fortifier: Fortifier) -> void:
 	id = EffectID.INVINCIBLE;
 	fortifier = _fortifier
 	fortifier.resetting.connect(func () -> void: active = false)
+	fortifier.killed.connect(func () -> void: active = false)
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
