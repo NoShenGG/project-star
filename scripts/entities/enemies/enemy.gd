cdc5343112ee088df78542222ffd23e99c3fdcbd
Fire551 <52729867+Fire551@users.noreply.github.com>
2025-10-10 01:29:36 -0400

    cleaned up enemy for movement

--- scripts/entities/enemies/enemy.gd
+++ scripts/entities/enemies/enemy.gd
@@ -2,10 +2,22 @@
 class_name Enemy extends Entity
 
 @onready var navigation_agent: NavigationAgent3D = $NavigationAgent3D
-@export var vision_radius: float
-@export var attack_radius: float
+@export var vision_radius: float = 1
+@export var attack_radius: float = 1
 # TODO: This is jank, use state machine when completed
 @export var is_attacking: bool
+
+signal move
+signal idle
+
+var moving : bool : 
+	set(value):
+		if (moving != value):
+			if (value):
+				move.emit()
+			else:
+				idle.emit()
+		moving = value
 
 func _init() -> void:
 	faction = Faction.HOSTILE
@@ -14,6 +26,14 @@ func _ready() -> void:
 	navigation_agent.velocity_computed.connect(Callable(_on_velocity_computed))
 
 func set_movement_target(movement_target: Vector3):
+<<<<<<< Updated upstream
+=======
+	var move_distance : float = global_position.slide(Vector3.UP).distance_to(movement_target.slide(Vector3.UP))
+	print(move_distance)
+	if (!moving and move_distance > 1):
+		moving = true
+		print("moving")
+>>>>>>> Stashed changes
 	navigation_agent.target_position = movement_target
 
 func attack() -> void:
@@ -27,13 +47,17 @@ func _process(delta: float) -> void:
 		attack()
 
 func _physics_process(_delta: float) -> void:
+	if (death): return
+	
 	if NavigationServer3D.map_get_iteration_id(navigation_agent.get_navigation_map()) == 0:
 		return
 	if navigation_agent.is_navigation_finished():
+		moving = false
 		return
 	if is_attacking:
+		moving = false
 		return
-		
+	
 	var next_path_position: Vector3 = navigation_agent.get_next_path_position()
 	var new_velocity: Vector3 = global_position.direction_to(next_path_position)
 	if navigation_agent.avoidance_enabled:
