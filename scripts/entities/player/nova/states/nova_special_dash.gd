3d96e889688f57a155dbaa63a753488d72877e95
Christian Dutton <christianedutton@gmail.com>
2025-10-18 03:38:41 -0400

    Fixed special damage application, updated cooldown indicator

--- scripts/entities/player/nova/states/nova_special_dash.gd
+++ scripts/entities/player/nova/states/nova_special_dash.gd
@@ -15,9 +15,11 @@ func enter(_prev_state: String, data := {}) -> void:
 	super(_prev_state, data)
 	nova.give_dash()
 	nova.dash(nova.special_dash_dist, false)
-	
 	await_frame()
 
+
 func await_frame() -> void:
+	# Wait two frames to allow hitbox to refresh
+	await get_tree().physics_frame
 	await get_tree().physics_frame
 	do_damage()
