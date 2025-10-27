148a0507248f6b28e9b5beed5651c00b07090dec
Ian Rogers <therazzberry25@gmail.com>
2025-10-27 18:34:41 -0400

    Adding rotation to aggro and shielding
    Adds code to the aggro and shielding states of the fortiifer to make it rotate towards the player.

--- scenes/dev/ian_r/fortifier_states/fortifier_shielding.gd
+++ scenes/dev/ian_r/fortifier_states/fortifier_shielding.gd
@@ -1,6 +1,8 @@
 class_name Shielding extends FortifierState
 
 
+@export var rotate_speed : float = 10
+@export var attack_distance : float = 1
 var friends: Array[Enemy] = []
 signal shield_enemy
 
@@ -14,7 +16,9 @@ func update(_delta: float) -> void:
 
 func physics_update(delta: float) -> void:
 	## goal: have the fortifier face the player while in this state
-	pass
+	var dir : Vector3 = (enemy.global_position - GameManager.curr_player.global_position).normalized().slide(Vector3.UP) * attack_distance
+	enemy.rotate_y(enemy.global_basis.z.signed_angle_to(dir, Vector3.UP) * delta * rotate_speed)
+	# end of goal
 
 func end() -> void:
 	pass
