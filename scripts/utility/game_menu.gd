a3106251338c3e2ef724efd7d47a5441af0ba4c4
Fire551 <52729867+Fire551@users.noreply.github.com>
2025-11-21 04:50:44 -0500

    health menu fix, minor playermanager and kobold cleanup

---
+++ scripts/utility/game_menu.gd
@@ -0,0 +1,24 @@
+extends Menu
+
+@export var overridable_menus : Array[Menu]
+
+func _enter_tree() -> void:
+	pass
+
+func _exit_tree() -> void:
+	pass
+
+func _ready() -> void:
+	for menu in overridable_menus:
+		menu.menu_closed.connect(check_should_open)
+		menu.menu_shown.connect(close_game_menu)
+
+func close_game_menu():
+	close()
+
+func check_should_open():
+	for menu in overridable_menus:
+		if (menu.is_open):
+			return
+	
+	open()
