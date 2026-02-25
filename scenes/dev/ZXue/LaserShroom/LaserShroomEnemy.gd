d10bec89b8df550448a6483150dd8f8672af8a4f
antho <antho@SKZL-ROG>
2026-02-25 08:07:59 -0500

    several VFX reimplemented, added spawning VFX, laser VFX, fixed gunshroom VFX, idk man

--- scenes/dev/ZXue/LaserShroom/LaserShroomEnemy.gd
+++ scenes/dev/ZXue/LaserShroom/LaserShroomEnemy.gd
@@ -7,6 +7,7 @@ var player_ref : Player:
 @export var hide_speed : float = 7.0
 @export var aim_mesh: MeshInstance3D
 @export var fire_mesh: MeshInstance3D
+@export var warning_particles : GPUParticles3D
 	
 func _physics_process(_delta: float) -> void:
 	super(_delta)
@@ -22,9 +23,12 @@ func switchMesh(status:int) -> void:
 	if (status == 1):
 		aim_mesh.visible = true
 		fire_mesh.visible = false
+		warning_particles.emitting = true
 	elif (status == 2):
 		aim_mesh.visible = false
 		fire_mesh.visible = true
+		warning_particles.emitting = false
 	else:
 		aim_mesh.visible = false
 		fire_mesh.visible = false
+		warning_particles.emitting = false
