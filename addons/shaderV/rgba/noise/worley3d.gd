3859e76975df72178546e064f3fe50a376df56a6
Fire551 <52729867+Fire551@users.noreply.github.com>
2026-05-12 16:11:20 -0400

    more ports to 4.7.b2

--- addons/shaderV/rgba/noise/worley3d.gd
+++ addons/shaderV/rgba/noise/worley3d.gd
@@ -38,6 +38,8 @@ func _get_input_port_name(port: int):
 			return "jitter"
 		4:
 			return "time"
+		_:
+			return "time"
 
 func _get_input_port_type(port: int):
 	match port:
@@ -51,6 +53,8 @@ func _get_input_port_type(port: int):
 			return VisualShaderNode.PORT_TYPE_SCALAR
 		4:
 			return VisualShaderNode.PORT_TYPE_SCALAR
+		_:
+			return VisualShaderNode.PORT_TYPE_SCALAR
 
 func _get_output_port_count() -> int:
 	return 2
@@ -61,12 +65,16 @@ func _get_output_port_name(port: int):
 			return "F1"
 		1:
 			return "F2"
+		_:
+			return "F2"
 
 func _get_output_port_type(port: int):
 	match port:
 		0:
 			return VisualShaderNode.PORT_TYPE_SCALAR
 		1:
+			return VisualShaderNode.PORT_TYPE_SCALAR
+		_:
 			return VisualShaderNode.PORT_TYPE_SCALAR
 
 func _get_global_code(mode):
