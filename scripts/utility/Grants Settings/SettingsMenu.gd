1ab44eb22f1aace78e65c4e561062573691a8337
Fire551 <52729867+Fire551@users.noreply.github.com>
2026-05-14 18:03:22 -0400

    added in volume control. FUCKING HELL

--- scripts/utility/Grants Settings/SettingsMenu.gd
+++ scripts/utility/Grants Settings/SettingsMenu.gd
@@ -1,7 +1,7 @@
 @tool
 class_name Settings extends Node
 
-var data : SettingsData = SettingsData.new()
+static var data : SettingsData = SettingsData.new()
 
 func _enter_tree() -> void:
 	unique_name_in_owner = true
@@ -19,18 +19,31 @@ func set_bus_volume(index : int, volume_percent : float):
 	var bus_name : String = "SFX"
 	match index:
 		0:
-			bus_name = "bus:/SFX"
+			bus_name = "{c56923f2-facf-4234-8503-244eee038ce3}"
 		1:
-			bus_name = "bus:/Music"
+			bus_name = "{91e7148e-3e78-4553-a738-7b82edaae1a0}"
 		2:
-			bus_name = "bus:/UI"
+			bus_name = "{613f8ce6-2727-4ecf-a9c2-159cb4caf311}"
 		3:
-			bus_name = "bus:/Ambiance"
-	print(bus_name)
+			bus_name = "{2533f5e6-3755-4921-850d-695a7c7f6a3f}"
+		4:
+			bus_name = "{40cf6196-4f6a-475c-bd2b-3207375401d5}"
+	
+	## because paths for fmod are ridiuclously bugged for some reason
 	for bus in FmodServer.get_all_buses():
-		print("pathhh  " + bus.get_path())
-	print("path i s   " + (FmodServer.get_all_buses()[1] as FmodBus).get_path())
+		if ((bus as FmodBus).get_guid() == bus_name):
+			print(volume_percent)
+			bus.volume = volume_percent#linear_to_db(1 - volume_percent)
+	for bus in FmodServer.get_all_vca():
+		if ((bus as FmodVCA).get_guid() != bus_name):
+			pass#bus.volume = linear_to_db(volume_percent)
+	
+	return
+	## WHY IS EVERYTHING BULLSHIT
+	#print("path i s   " + (FmodServer.get_all_buses()[1] as FmodVCA).get_path())
 	var bus : FmodBus = FmodServer.get_bus(bus_name)
+	FmodServer.get_vca("vca:/Ambiance").volume = 5
+	FmodServer.get_bus("bus:/Ambiance").volume = 5
 	if (bus):
 		print("setting volume!!!")
 		bus.volume = linear_to_db(volume_percent)
--- scripts/utility/Grants Settings/VolumeSliderManager.gd
+++ scripts/utility/Grants Settings/VolumeSliderManager.gd
@@ -1,15 +1,17 @@
 extends Node
 
+@export var master_slider : Slider
 @export var SFX_slider : Slider
 @export var music_slider : Slider
 @export var UI_slider : Slider
 @export var ambiance_slider : Slider
 
 func _ready() -> void:
-	SFX_slider.value_changed.connect(func(value : float): set_volume(0, value))
-	music_slider.value_changed.connect(func(value : float): set_volume(1, value))
-	UI_slider.value_changed.connect(func(value : float): set_volume(2, value))
-	ambiance_slider.value_changed.connect(func(value : float): set_volume(3, value))
+	master_slider.value_changed.connect(func(value : float): set_volume(0, value))
+	SFX_slider.value_changed.connect(func(value : float): set_volume(1, value))
+	music_slider.value_changed.connect(func(value : float): set_volume(2, value))
+	UI_slider.value_changed.connect(func(value : float): set_volume(3, value))
+	ambiance_slider.value_changed.connect(func(value : float): set_volume(4, value))
 
 func set_volume(index : int, value : float):
 	(%Settings as Settings).set_bus_volume(index, value)
